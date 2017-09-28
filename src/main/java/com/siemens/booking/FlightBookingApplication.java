70b74a9ca65509a6df656183d8f0688c66d90c53
dxghg8 <dxghg8@pl1inpun0386nb.net.plm.eds.com>
2017-09-28 10:52:04 +0530

    commit

---
+++ src/main/java/com/siemens/booking/FlightBookingApplication.java
@@ -0,0 +1,25 @@
+package com.siemens.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FlightBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingApplication.class, args);
+	}
+}
+
+@RestController
+class ServiceInstanceRestController {	
+    @RequestMapping("/")
+    public ResponseEntity<String> test() {
+    	
+        return new ResponseEntity<String>("Hello World!", HttpStatus.CREATED);
+    }
+} 
